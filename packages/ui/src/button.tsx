915ba4c833d7f7b4e9b94cf6f7e96a0a4fbca602
Chathura Samarajeewa <chathura.samarajeewa@gmail.com>
2025-09-02 22:40:02 +0530

    fix: fix the tailwindcss not correctly attached issue

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -8,10 +8,13 @@ interface ButtonProps {
   appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className = "", appName }: ButtonProps) => {
+  const defaultClasses =
+    "appearance-none rounded-full h-12 px-5 border border-gray-200 dark:border-gray-800 transition-all cursor-pointer flex items-center justify-center text-base font-medium bg-transparent min-w-[180px] hover:bg-gray-100 dark:hover:bg-gray-800";
+
   return (
     <button
-      className={className}
+      className={`${defaultClasses} ${className}`}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
