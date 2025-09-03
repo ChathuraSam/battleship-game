7f547d4bb433600ad8e1222fd3c26eab1e345ec7
Chathura Samarajeewa <chathura.samarajeewa@gmail.com>
2025-09-03 17:39:33 +0530

    fix: webSocket connection losing issue fix

---
+++ apps/frontend/hooks/useSocket.ts
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useSocketContext } from "../contexts/SocketContext";
+
+interface Move {
+  x: number;
+  y: number;
+}
+
+type OnOpponentMove = (move: Move) => void;
+
+export default function useSocket(
+  gameId: string,
+  onOpponentMove?: OnOpponentMove
+) {
+  const { socket } = useSocketContext();
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.emit("joinGame", gameId);
+
+    socket.on("playerJoined", (data) => {
+      console.log("Another player joined:", data);
+    });
+
+    socket.on("opponentMove", (move: Move) => {
+      if (onOpponentMove) onOpponentMove(move);
+    });
+
+    return () => {
+      socket.off("playerJoined");
+      socket.off("opponentMove");
+    };
+  }, [socket, gameId, onOpponentMove]);
+
+  const makeMove = (x: number, y: number) => {
+    socket?.emit("makeMove", { gameId, x, y });
+  };
+
+  return { makeMove };
+}
